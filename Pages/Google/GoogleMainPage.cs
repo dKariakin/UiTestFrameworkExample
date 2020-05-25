0e43de084f0e6f800b2dca530c95b5fef6577bb0
dKaryakin <karyakin.dmitry@gmail.com>
2020-05-26 03:29:49 +0600

    framework has changed to net.core 3.1
    TOFIX test running

--- Pages/Google/GoogleMainPage.cs
+++ Pages/Google/GoogleMainPage.cs
@@ -1,23 +1,16 @@
 ﻿using Extensions.Pages.Base;
 using OpenQA.Selenium;
-using OpenQA.Selenium.Support.PageObjects;
 
 namespace Pages.Google
 {
   public sealed class GoogleMainPage : PagePrototype, IPagePrototype
   {
-    [FindsBy(How = How.Name, Using = "q")]
-    [CacheLookup]
-    public IWebElement SearchString;
+    public IWebElement SearchString => _webDriver.FindElement(By.Name("q"));
 
-    [FindsBy(How = How.XPath, Using = "(//input[contains(@value, 'Google')])[2]")]
-    [CacheLookup]
-    public IWebElement SearchButton;
+    public IWebElement SearchButton => _webDriver.FindElement(By.XPath("(//input[contains(@value, 'Google')])[2]"));
 
     public GoogleMainPage(IWebDriver webDriver) : base(webDriver, PageNames.GoogleMainPage, "http://www.google.com")
     {
-      PageFactory.InitElements(_webDriver, this);
-      
       SetElements(new (string, IWebElement)[]
       {
         ("search string", SearchString),
--- Pages/Google/GoogleSearchResultPage.cs
+++ Pages/Google/GoogleSearchResultPage.cs
@@ -1,19 +1,14 @@
 ﻿using Extensions.Pages.Base;
 using OpenQA.Selenium;
-using OpenQA.Selenium.Support.PageObjects;
 
 namespace Pages.Google
 {
   public sealed class GoogleSearchResultPage : PagePrototype, IPagePrototype
   {
-    // put element description here
-    [FindsBy(How = How.XPath, Using = "//div[@id='res']//div[@class='r']//h3")]
-    public IWebElement searchResults;
+    public IWebElement searchResults => _webDriver.FindElement(By.XPath("//div[@id='res']//div[@class='r']//h3"));
 
     public GoogleSearchResultPage(IWebDriver webDriver) : base(webDriver, PageNames.GoogleSearchResultPage)
     {
-      PageFactory.InitElements(_webDriver, this);
-
       SetElements(new (string, IWebElement)[]
       {
         ("search result", searchResults)
